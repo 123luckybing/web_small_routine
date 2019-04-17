fde11b7f6a8634e89583d89e6ea796cffd78d450
周芳冰 <zhoufangbing@daojia-inc.com>
2019-04-17 13:15:56 +0800

    上门收书

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,45 +1,130 @@
 // pages/view/view.js
+import WxValidate from '../../utils/WxValidate'
+const app = getApp()
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-    loadingHide: true,
-    loadingText: "加载中"
+   kindList: [],
   },
-
-  //http://lbs.qq.com/tool/getpoint/ 坐标拾取器
-  //* 本代码由起航网络工作室制作，认准淘宝旺旺唯一ID：LZC940217
-  click: function (e) {
-    wx.openLocation({
-      latitude: 40.281150,
-      longitude: 111.197160,
-      scale: 18,
-      name: '老三传媒工作室',
-      address: '内蒙古呼和浩特市托克托县光明路'
+  bindPickerChange(e) {
+    this.setData({
+      index: e.detail.value
     })
   },
-  teltoUs: function () {
-    wx.makePhoneCall({
-      phoneNumber: '15354880068' //仅为示例，并非真实的电话号码
+  //报错 
+  showToast(error) {
+    wx.showToast({
+      title: error.msg,
+      icon: 'none'
     })
   },
-  onShareAppMessage: function () {
-    return {
-      title: '托县老三纹身',
-      desc: '专注于纹身，纹眉等业务。',
-      path: "pages/home/home",
+  initValidate() {
+    const rules = {
+      name: {
+        required: true
+      },
+      number: {
+        required: true,
+      },
+      kindName: {
+        required: true,
+      },
+      seller: {
+        required: true,
+      },
+      mobileNo: {
+        required: true,
+      },
+      address: {
+        required: true,
+      }
     }
+    const messages = {
+      name: {
+        required: '请输入书名',
+      },
+      number: {
+        required: '请输入数量',
+      },
+      kindName: {
+        required: '请输入种类',
+      },
+      seller: {
+        required: '请输入您的姓名',
+      },
+      mobileNo: {
+        required: '请输入您的电话号码',
+        intType: '请输入11位标准号码'
+      },
+      address: {
+        required: '请输入上门地址',
+      }
+    }
+    this.WxValidate = new WxValidate(rules, messages)
   },
-  /**
+/**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    var that = this;
-    that.setData({ loadingHide: false });
-    setTimeout(function () {
-      that.setData({ loadingHide: true });
-    }, 1000)
-  }
+    this.initValidate()
+    const that = this
+    wx.request({
+      url: app.globalData.URL + '/kind/list',
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      data: {},
+      method: 'POST',
+      success(res) {
+        that.setData({
+          kindList: res.data.list
+        })
+      }
+    })
+  },
+  formSubmit(e) {
+    const params = e.detail.value
+    const that = this
+    // 校验表单
+    if (!this.WxValidate.checkForm(params)) {
+      const error = this.WxValidate.errorList[0]
+      this.showToast(error)
+      return false
+    } else {
+      console.log(e.detail.value)
+      wx.request({
+        url: app.globalData.URL + '/recycle/add',
+        header: {
+          'content-type': 'application/json' // 默认值
+        },
+        data: e.detail.value,
+        method: 'POST',
+        success(res) {
+          if(res.data.code === 0) {
+            wx.showToast({
+              title: res.data.msg,
+              duration: 3000,
+              mask:true
+            })
+            setTimeout(function(){
+              wx.hideToast()
+            }, 2000)
+          } else {
+            wx.showToast({
+              title: res.data.msg,
+              duration: 1000,
+              mask:true
+            })
+            setTimeout(function(){
+              wx.hideToast()
+            }, 2000)
+          }
+          console.log(res)
+        }
+      })
+    }
+  },
+  formReset() {},
 })
